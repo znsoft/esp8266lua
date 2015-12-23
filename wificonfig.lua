3b3350311f1479ccb825a8eef102a94c94e5b316
znsoft <zn@bk.ru>
2015-12-23 23:30:36 +1000

    Авто переподключение к вифи + доработка ридми

--- wificonfig.lua
+++ wificonfig.lua
@@ -2,3 +2,8 @@ wifi.setmode(wifi.STATIONAP)
 wifi.ap.config({ssid="192.168.4.1",pwd="APpassword"})
 wifi.sta.config("znlink","STApassword")
 wifi.sta.autoconnect(1)
+tmr.alarm(4, 3000, 1, function()
+       if wifi.sta.getip() == nil then
+       wifi.sta.connect()
+       end
+end)
